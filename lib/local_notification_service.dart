70c1a4ac5be73d54511cb8b7deace27c2afdf0f3
rahulgupta13130 <rahulgupta13130@gmail.com>
2023-02-16 11:47:22 +0530

    not neede this file

--- lib/local_notification_service.dart
+++ lib/local_notification_service.dart
@@ -1,59 +1,59 @@
-import 'dart:developer';
+// import 'dart:developer';
 
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class LocalNotificationService {
-  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
-      FlutterLocalNotificationsPlugin();
+// class LocalNotificationService {
+//   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+//       FlutterLocalNotificationsPlugin();
 
-  static void initialize(BuildContext context) {
-    const InitializationSettings initializationSettings =
-        InitializationSettings(
-            android: AndroidInitializationSettings("@mipmap/ic_launcher"));
+//   static void initialize(BuildContext context) {
+//     const InitializationSettings initializationSettings =
+//         InitializationSettings(
+//             android: AndroidInitializationSettings("@mipmap/ic_launcher"));
 
-    _notificationsPlugin.initialize(initializationSettings,
-    onDidReceiveNotificationResponse: (route) {
+//     _notificationsPlugin.initialize(initializationSettings,
+//     onDidReceiveNotificationResponse: (route) {
 
-      if(route != null){
-        // log(route.payload);
-        // Navigator.of(context).pushNamed(route.payload);
+//       if(route != null){
+//         // log(route.payload);
+//         // Navigator.of(context).pushNamed(route.payload);
       
-    }}
-    // onSelectNotification: (String? route
-    // ) async{
-    //   if(route != null){
-    //     Navigator.of(context).pushNamed(route);
-    //   }
-    // }
-    );
-  }
+//     }}
+//     // onSelectNotification: (String? route
+//     // ) async{
+//     //   if(route != null){
+//     //     Navigator.of(context).pushNamed(route);
+//     //   }
+//     // }
+//     );
+//   }
 
-  static void display(RemoteMessage message) async {
+//   static void display(RemoteMessage message) async {
 
-    try {
-      final id = DateTime.now().millisecondsSinceEpoch ~/1000;
+//     try {
+//       final id = DateTime.now().millisecondsSinceEpoch ~/1000;
 
-      const NotificationDetails notificationDetails = NotificationDetails(
-        android: AndroidNotificationDetails(
-          "high_importance_channel",
-          "High Importance Notifications",
-          importance: Importance.max,
-          priority: Priority.high,
-        )
-      );
+//       const NotificationDetails notificationDetails = NotificationDetails(
+//         android: AndroidNotificationDetails(
+//           "high_importance_channel",
+//           "High Importance Notifications",
+//           importance: Importance.max,
+//           priority: Priority.high,
+//         )
+//       );
 
 
-      await _notificationsPlugin.show(
-        id,
-        message.notification!.title,
-        message.notification!.body,
-        notificationDetails,
-        payload: message.data["route"],
-      );
-    } on Exception catch (e) {
-      print(e);
-    }
-  }
-}
+//       await _notificationsPlugin.show(
+//         id,
+//         message.notification!.title,
+//         message.notification!.body,
+//         notificationDetails,
+//         payload: message.data["route"],
+//       );
+//     } on Exception catch (e) {
+//       print(e);
+//     }
+//   }
+// }
